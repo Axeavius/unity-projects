71849bd54e7df3fbd20474fcb9d588f0606ebe75
Axeavius <axeavius@gmail.com>
2017-12-21 17:27:20 -0800

    Roll a Ball
    Adding Roll a Ball tutorial project

---
+++ Roll a Ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+
+    private Vector3 offest;
+
+    // Use this for initialization
+    void Start () {
+        offest = transform.position - player.transform.position;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        //transform.position = player.transform.position + offest;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offest;
+    }
+}
