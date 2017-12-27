373b862211e6fa11a473dd66c2abb8e2162217ca
Axeavius <axeavius@gmail.com>
2017-12-27 15:51:45 -0800

    Updated 2D UFO tutorial project

--- Tutorials/_macOS/2D UFO/Assets/Scripts/Rotator.cs
+++ Tutorials/_macOS/2D UFO/Assets/Scripts/Rotator.cs
@@ -2,11 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotator : MonoBehaviour {
-
-
-
-
+public class Rotator : MonoBehaviour
+{
 	void Update()
 	{
 		transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
